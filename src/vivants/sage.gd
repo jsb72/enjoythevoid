a5b1d016201ba213d3c536e3464c79104597dbfa
incon-science <1conscienceofficiel@gmail.com>
2026-04-15 14:22:33 +0200

    Refactor project structure and update scene configurations
    - Removed unused resources from filesystem update.
    - Updated project metadata to reflect recent file changes and reorganized script paths.
    - Adjusted camera positions and properties in room_blueprint scene for better alignment.
    - Modified sage scene to include new animation behavior and adjusted RichTextLabel properties.
    - Enhanced script editor cache settings for improved usability.
    - Updated global script class cache to reflect new class structures and paths.
    - Adjusted player and camera positions in the level blueprint for better gameplay experience.

--- src/vivants/sage.gd
+++ src/vivants/sage.gd
@@ -16,8 +16,11 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		if !iswaken:
-			animated_sprite_2d.play("wakeup")
+			var tween2 = get_tree().create_tween()
+			tween2.tween_property(rich_text_label, "modulate:a", 1.0, 1.0)
 			
+			animated_sprite_2d.play("wakeup")
+			await get_tree().create_timer(1).timeout
 			var tween = get_tree().create_tween()
 			tween.tween_property(rich_text_label, "modulate:a", 0.0, 10.0)
 			
