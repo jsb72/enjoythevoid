1b31d0245d566a859a6a77e7b3ea2caee6aea943
incon-science <1conscienceofficiel@gmail.com>
2026-04-20 23:12:14 +0200

    Refactor and update various game assets and scripts
    - Adjusted script editor cache settings for improved usability.
    - Modified edit state configuration for the spider scene, enabling visibility and updating selected nodes.
    - Updated folding configuration for the spider scene to include additional nodes.
    - Removed deprecated window size mode from project configuration.
    - Revised text content in meta room piece scene for clarity and conciseness.
    - Adjusted player and camera positions in room blueprint scene for better gameplay experience.
    - Refactored spider script to improve collision detection and movement logic.
    - Updated spider scene properties, including size adjustments and motion mode changes.

--- src/vivants/spider.gd
+++ src/vivants/spider.gd
@@ -6,42 +6,36 @@ extends CharacterBody2D
 
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-
 @onready var timer: Timer = $Timer
 
 @onready var right: RayCast2D = $right
 @onready var left: RayCast2D = $left
-
-
 
 const SPEED = 50.0
 
 var direction : int = 0
 var rng = RandomNumberGenerator.new()
 
-func getcollisionbodyright():
-	if right.is_colliding():
-		var collidobj = right.get_collider()
-		if collidobj is Player or collidobj is Spider :
-			return collidobj
-	return null
-			
-func getcollisionbodyleft():
-	if left.is_colliding():
-		var collidobj = left.get_collider()
-		if collidobj is Player or collidobj is Spider :
-			return collidobj
-	return null
-			
-
-func is_attacking()->bool:
-	return animated_sprite_2d.animation == "attack" and animated_sprite_2d.is_playing()
-	
 func _physics_process(delta: float) -> void:
+	#atk_logik()
+	if !is_attacking():move_logic(delta)
 	
-	var bodycolright :CharacterBody2D=getcollisionbodyright()
-	var bodycolleft :CharacterBody2D=getcollisionbodyleft()
+	move_animation()
 	
+func move_logic(delta)->void:
+	"""if not is_on_floor():
+		velocity += get_gravity() * delta"""
+
+	if direction :
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	move_and_slide()
+	
+func atk_logik()->void:
+	var bodycolright :CharacterBody2D=getcollisionbody(right)
+	var bodycolleft :CharacterBody2D=getcollisionbody(left)
 	
 	if bodycolright !=null or bodycolleft !=null:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -56,32 +50,30 @@ func _physics_process(delta: float) -> void:
 			spiderrendu.scale.x = 1
 			await get_tree().create_timer(0.1).timeout
 			bodycolright.position.x += 2
-	elif !is_attacking():
+			
+func move_animation()->void:
+	if velocity.x == 0 :
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("walk")
+	if velocity.x < 0 :
+		spiderrendu.scale.x = -1
+	if velocity.x > 0 :
+		spiderrendu.scale.x = 1
+	if is_on_ceiling():
+		spiderrendu.scale.y = -1
 		
 		
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.x == 0 :
-			animated_sprite_2d.play("idle")
-		else:
-			animated_sprite_2d.play("walk")
-		if velocity.x < 0 :
-			spiderrendu.scale.x = -1
-		if velocity.x > 0 :
-			spiderrendu.scale.x = 1
-			
-			
-			
-			
-			
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-				
-	move_and_slide()
+func is_attacking()->bool:
+	return animated_sprite_2d.animation == "attack" and animated_sprite_2d.is_playing()
 	
+func getcollisionbody(rcast:RayCast2D):
+	if rcast.is_colliding():
+		var collidobj = rcast.get_collider()
+		if collidobj is Player or collidobj is Spider :
+			return collidobj
+	return null
+
 func getCollisionSurface(rcast:RayCast2D):
 	if rcast.is_colliding():
 		var collidobj = rcast.get_collider()
@@ -90,5 +82,5 @@ func getCollisionSurface(rcast:RayCast2D):
 	return null
 
 func _on_timer_timeout() -> void:
-	direction = rng.randi_range(-1, 1)
+	direction = rng.randi_range(1, 1)
 	timer.start()
