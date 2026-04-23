46fa383d46284bb2c12a60b88e4f582d422d9932
incon-science <1conscienceofficiel@gmail.com>
2026-04-23 19:58:08 +0200

    Refactor scene configurations and update node properties; enhance fly behavior with additional ray casts and collision handling

--- src/vivants/fly.gd
+++ src/vivants/fly.gd
@@ -4,30 +4,31 @@ extends CharacterBody2D
 @onready var timer: Timer = $Timer
 @onready var bomb: RigidBody2D = $bomb
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var ray_cast_2d_2: RayCast2D = $RayCast2D2
+
 @onready var rendu: Node2D = $rendu
 
 @onready var ancrerouge: ColorRect = $RopeAnchor/ancrerouge
 
+@export var free_zone:Area2D
 
 const SPEED = 75.0
 
-var direction : int = 0
+var directionx : int = 0
+var directiony : int = 0
 var rng = RandomNumberGenerator.new()
 var rng2 = RandomNumberGenerator.new()
 
 var throwing_bomb : bool = false
-var initial_pos : Vector2
 var new_bomb : RigidBody2D
 func _ready() -> void:
-	initial_pos=global_position
 	new_bomb=bomb
 	
 func _physics_process(delta: float) -> void:
 	new_bomb.global_position=ancrerouge.global_position
-	#new_bomb.global_position.y+=22
 	new_bomb.gravity_scale=0
 	
-	if ray_cast_2d.is_colliding() and !throwing_bomb:#collision mask only collide mask 2 (player is layer collision 2)
+	if (ray_cast_2d.is_colliding() or ray_cast_2d_2.is_colliding()) and !throwing_bomb:#collision mask only collide mask 2 (player is layer collision 2)
 		throwing_bomb=true 
 		
 		new_bomb.gravity_scale=1
@@ -53,17 +54,15 @@ func _physics_process(delta: float) -> void:
 	
 	
 
-	if global_position.x > initial_pos.x + 600:
-		if direction > 0 :
-			direction = 0
-	if global_position.x < initial_pos.x - 600:
-		if direction < 0 :
-			direction = 0
 		
-	if direction:
-		velocity.x = direction * SPEED
+	var vec_tmp = Vector2(directionx,directiony)
+	vec_tmp = vec_tmp.normalized()
+	if vec_tmp != Vector2(0,0):
+		velocity.x = vec_tmp.x * SPEED
+		velocity.y = vec_tmp.y * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	if velocity.x < 0 :
 		rendu.scale.x=1
 	if velocity.x > 0 :
@@ -71,9 +70,15 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-		
+	if !free_zone.overlaps_body(self) and!timer.paused:
+		timer.paused=true
+		directionx=-directionx
+		directiony=-directiony
+		await get_tree().create_timer(1).timeout
+		timer.paused=false
 
 
 func _on_timer_timeout() -> void:
-	direction = rng.randi_range(-1, 1)
+	directionx = rng.randi_range(-1, 1)
+	directiony = rng.randi_range(-1, 1)
 	timer.start()
